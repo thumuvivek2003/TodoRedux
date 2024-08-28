c0fc682a575baea570b4b81ebef3c2b6e619f28e
VIVEK THUMU <thumuvivek2003@gmail.com>
2024-08-28 15:35:35 +0530

    Add React Icons and align to mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import './Header.css';
-
+import { MdOutlineDomainVerification,MdOutlineBallot,MdOutlinePendingActions } from "react-icons/md";
 const Header = () => {
   const todos = useSelector((state) => state.todos);
   const totalCount = todos.length;
@@ -13,13 +13,24 @@ const Header = () => {
       <h1 style={{color:'white'}}>Todo List</h1>
       <div className="badges">
         <div className="badge total">
-          <span>Total Todos:</span> {totalCount}
+          <span style={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>
+            <MdOutlineBallot size={30}/>
+            {totalCount}
+          </span>
         </div>
+
         <div className="badge completed">
-          <span>Completed:</span> {completedCount}
+          <span style={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>
+            <MdOutlineDomainVerification size={30}/>
+            {completedCount}
+          </span>
         </div>
+
         <div className="badge remaining">
-          <span>Remaining:</span> {remainingCount}
+          <span style={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>
+            <MdOutlinePendingActions size={30}/>
+            {remainingCount}
+          </span>
         </div>
       </div>
     </header>
